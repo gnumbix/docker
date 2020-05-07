3d67a77796048742a0aa7ad219edce8d0c486d6b
Gnumbix <gnumbix@users.noreply.github.com>
2020-05-07 10:50:51 +0300

    Update build.sh

--- teamcity_agent/build.sh
+++ teamcity_agent/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -LfsSo ./buildAgent.zip http://172.16.134.8/update/buildAgent.zip;
+curl -LfsSo ./buildAgent.zip http://127.0.0.1/update/buildAgent.zip;
 mkdir dist
 unzip buildAgent.zip -d dist/buildagent
 mv dist/buildagent/conf dist/buildagent/conf_dist
